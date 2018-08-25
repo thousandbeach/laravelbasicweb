4fab77c32875b566647eb246ef084b7ea09f326c
takabo <thousandbeach.td@gmail.com>
2018-08-26 03:49:37 +0900

    contact.blade.php label日本語化

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,15 +4,15 @@
   <h1>お問い合わせ</h1>
   {!! Form::open(['url' => 'contact/submit']) !!}
     <div class="form-group">
-      {{Form::label('name', 'Name')}}
+      {{Form::label('name', 'お名前')}}
       {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'お名前'])}}
     </div>
     <div class="form-group">
-      {{Form::label('email', 'E-Mail Address')}}
+      {{Form::label('email', 'メールアドレス')}}
       {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'メールアドレス'])}}
     </div>
     <div class="form-group">
-      {{Form::label('message', 'Message')}}
+      {{Form::label('message', 'メッセージ')}}
       {{Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'メッセージ'])}}
     </div>
     <div>
