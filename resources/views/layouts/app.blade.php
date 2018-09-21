2ea5bb6074d5a1fc8d2f41842263c3494e268f13
takabo <thousandbeach.td@gmail.com>
2018-09-21 15:15:48 +0900

    BootstrapのJSとjQueryをapp.blade.phpに追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
     <meta charset="utf-8">
     <title>larabasicweb</title>
     <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <!-- Bootstrap CSS-->
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
     @include('inc.navbar')
